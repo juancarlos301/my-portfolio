1014651a81f08fbaeff9c172959aba77f0f8a365
juancarlos301 <juancarlos.31.2002@gmail.com>
2022-06-21 15:41:13 -0500

    arreglo responsive

--- src/components/containerImag.jsx
+++ src/components/containerImag.jsx
@@ -4,36 +4,36 @@ import '../styles/ConImag.css'
 const ConImag = (props) => {
 
     return (
-        <div class="container">
-            <div class="box">
-                <div class="imgBx">
+        <div className="container">
+            <div className="box">
+                <div className="imgBx">
                     <img src={props.Imagen1} alt="" />
                 </div>
-                <div class="content">
+                <div className="content">
                     <div>
                         <h2>{props.title}</h2>
                         <p>{props.par1}</p>
                     </div>
                 </div>
             </div>
-            <div class="box">
-                <div class="imgBx">
+            <div className="box">
+                <div className="imgBx">
                     <img src={props.Imagen2} alt="" />
 
                 </div>
-                <div class="content">
+                <div className="content">
                     <div>
                         <h2>{props.title}</h2>
                         <p>{props.par2}</p>
                     </div>
                 </div>
             </div>
-            <div class="box">
-                <div class="imgBx">
+            <div className="box">
+                <div className="imgBx">
                     <img src={props.Imagen3} alt="" />
 
                 </div>
-                <div class="content">
+                <div className="content">
                     <div>
                         <h2>{props.title}</h2>
                         <p>{props.par3}</p>
